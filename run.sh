a11b9e1c930d1c98cd01e86c304204563ab24398
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-01-03 12:43:28 -0800

    docker container with docker socket from outside

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t codersos/image-creator-server . && docker run --rm codersos/image-creator-server
+docker build -t codersos/image-creator-server . && docker run --rm -v /var/run/docker.sock:/var/run/docker.sock codersos/image-creator-server
